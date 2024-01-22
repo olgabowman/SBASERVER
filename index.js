650da863a74b952fb4c7e86004a0aaee8bf158bd
olgabowman <olgabowman@gmail.com>
2024-01-22 14:29:32 -0500

    api added

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 
 const express = require('express');
 const router = express.Router();
-router.get('/api/sample', (req, res) => {
-  res.json({ message: 'This is a sample API route' });
+router.get('./treefacts', (req, res) => {
+  res.json({ message: 'This is my Tree api route' });
 });
 
 module.exports = router;
 
-router.get('/api/user/:id', (req, res) => {
-  const userId = req.params.id;
-  res.json({ message: `User ID: ${userId}` });
+router.get('./treefacts/:id', (req, res) => {
+  const treeId = req.params.id;
+  res.json({ message: `Tree ID: ${treeId}` });
 });
 
 
