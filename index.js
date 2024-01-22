aa00fda13ab490a0d7d0d6a7adea0fc730f72cd9
olgabowman <olgabowman@gmail.com>
2024-01-22 14:22:02 -0500

    more updates

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 
 const express = require('express');
+const router = express.Router();
+router.get('/api/sample', (req, res) => {
+  res.json({ message: 'This is a sample API route' });
+});
+
+module.exports = router;
+
+router.get('/api/user/:id', (req, res) => {
+  const userId = req.params.id;
+  res.json({ message: `User ID: ${userId}` });
+});
+
+
+app.use(express.json());
+
 
 const app = express();
 app.get('/', (req, res) => {
